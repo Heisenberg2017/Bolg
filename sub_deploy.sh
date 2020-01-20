e0a88997c8f9284659a3d1fa773f887e0c1c193f
heisenberg2017 <289957126@qq.com>
2020-01-20 14:56:43 +0800

    rebuilding site 2020年01月20日 14:56:42

---
+++ sub_deploy.sh
@@ -0,0 +1,10 @@
+# push to bolg
+cp -r * ../../OpenStackCode/Heisenberg2017.github.io/
+
+cd ../../OpenStackCode/Heisenberg2017.github.io/
+
+git add .
+
+git commit -m "$msg"
+
+git push origin master
