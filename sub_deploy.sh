6df46db4161c174643955833e9f6437d25581bf2
heisenberg2017 <289957126@qq.com>
2020-01-20 15:00:04 +0800

    rebuilding site 2020年01月20日 15:00:04

--- sub_deploy.sh
+++ sub_deploy.sh
@@ -1,4 +1,10 @@
 # push to bolg
+
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
 cp -r * ../../OpenStackCode/Heisenberg2017.github.io/
 
 cd ../../OpenStackCode/Heisenberg2017.github.io/
